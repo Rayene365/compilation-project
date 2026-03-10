3c7a9391eb6a607edd9c7d6b860067e43b7ab0ba
BOUZIDAdam <adam.bouzid@imt-atlantique.net>
2026-03-10 15:58:36 +0100

    type command du Pfx

--- pfx/basic/ast.ml
+++ pfx/basic/ast.ml
@@ -1,14 +1,27 @@
 type command =
-  DefineMe (* Question 4.1 *)
+  | Push of int
+  | Pop
+  | Swap
+  | Add
+  | Sub
+  | Mul
+  | Div
+  | Rem
 
 type program = int * command list
 
 (* add here all useful functions and types  related to the AST: for instance  string_of_ functions *)
 
 let string_of_command = function
-  | DefineMe -> "to be done"
+  | Push n -> Printf.sprintf "push %d" n
+  | Pop -> "pop"
+  | Swap -> "swap"
+  | Add -> "add"
+  | Sub -> "sub"
+  | Mul -> "mul"
+  | Div -> "div"
+  | Rem -> "rem"
 
 let string_of_commands cmds = String.concat " " (List.map string_of_command cmds)
 
 let string_of_program (args, cmds) = Printf.sprintf "%i args: %s\n" args (string_of_commands cmds)
-
