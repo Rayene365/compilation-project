7631d980e469c1ed87fb75600567da4e2183690d
Rayene365 <rayenabbassi007@gmail.com>
2026-03-17 11:52:27 +0100

    fonction generate expr/basic/toPfx.ml

--- expr/basic/toPfx.ml
+++ expr/basic/toPfx.ml
@@ -1,7 +1,24 @@
 open Ast
+open BasicPfx.Ast
+open BinOp
 
-let generate = function
-  | Const _ -> failwith "To implement"
-  | Binop(_,_,_) -> failwith "To implement"
-  | Uminus _ -> failwith "To implement"
-  | Var _ -> failwith "Not yet supported"
+let rec generate_into expr cont =
+  match expr with
+  | Const n ->
+      Push n :: cont
+  | Uminus e ->
+      generate_into e (Push 0 :: Sub :: cont)
+  | Binop (Badd, e1, e2) ->
+      generate_into e2 (generate_into e1 (Add :: cont))
+  | Binop (Bsub, e1, e2) ->
+      generate_into e2 (generate_into e1 (Sub :: cont))
+  | Binop (Bmul, e1, e2) ->
+      generate_into e2 (generate_into e1 (Mul :: cont))
+  | Binop (Bdiv, e1, e2) ->
+      generate_into e2 (generate_into e1 (Div :: cont))
+  | Binop (Bmod, e1, e2) ->
+      generate_into e2 (generate_into e1 (Rem :: cont))
+  | Var _ ->
+      failwith "variables are not supported yet"
+
+let generate expr = generate_into expr []
