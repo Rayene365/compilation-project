03aa989d8ff8b8c38a32cb4427d804ecd334d1e9
Rayene365 <rayenabbassi007@gmail.com>
2026-04-08 20:54:59 +0200

    Q13.5

--- expr/fun/toPfx.ml
+++ expr/fun/toPfx.ml
@@ -21,6 +21,29 @@ let opcode_of_binop = function
   | Bdiv -> Div
   | Bmod -> Rem
 
+let union xs ys =
+  List.fold_left (fun acc x -> if List.mem x acc then acc else acc @ [x]) xs ys
+
+let rec free_vars bound = function
+  | Const _ -> []
+  | Var var -> if List.mem var bound then [] else [var]
+  | Uminus e -> free_vars bound e
+  | Binop (_, e1, e2)
+  | App (e1, e2) ->
+      union (free_vars bound e1) (free_vars bound e2)
+  | Fun (var, body) ->
+      free_vars (var :: bound) body
+
+let function_env free_vars param =
+  List.mapi (fun depth var -> var, depth) free_vars @ [param, List.length free_vars]
+
+let capture_code env free_vars =
+  List.concat_map
+    (fun var ->
+      let depth = lookup env var + 1 in
+      [Push depth; Get; Append])
+    free_vars
+
 let rec generate_into env expr cont =
   match expr with
   | Const n ->
@@ -33,7 +56,10 @@ let rec generate_into env expr cont =
   | Binop (op, e1, e2) ->
       generate_into env e2 (generate_into (shift_env env) e1 (opcode_of_binop op :: cont))
   | Fun (var, body) ->
-      ExecSeq (generate_into (extend_env env var) body []) :: cont
+      let free = free_vars [var] body in
+      let body_env = function_env free var in
+      let body_code = generate_into body_env body [] in
+      ExecSeq body_code :: capture_code env free @ cont
   | App (e1, e2) ->
       generate_into env e2 (generate_into (shift_env env) e1 (Exec :: Swap :: Pop :: cont))
 
